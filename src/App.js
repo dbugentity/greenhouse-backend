11427dda78bc7be9332e630de4888bdf47cf4df3
vishwa <vishwa.goghari@kinship.co>
2023-07-08 11:12:52 -0400

    debug axios connection issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+<<<<<<< Updated upstream
 import './App.css';
 import JobBoard from './components/Greenhouse/JobBoard';
 import JobApplication from './components/Greenhouse/JobApplication';
+=======
+// @ts-nocheck
+import React from 'react';
+import styles from './App.module.scss';
+import JobBoard from "./components/Greenhouse/JobBoard";
+import JobApplication from './components/Greenhouse/JobApplication';
+import Introduction from './components/Greenhouse/Introduction/Introduction';
+import JobList from './components/Greenhouse/JobList';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
        <h3>K.</h3>
        <h3>Associates</h3>
   
       <JobBoard/>
       <JobApplication/>
+=======
+      
+      <section>
+        {/* <Introduction /> */}
+        {/* <JobBoard /> */}
+        {/* <JobApplication /> */}
+        <JobList />
+      </section>
+
+>>>>>>> Stashed changes
     </div>
   );
 }
