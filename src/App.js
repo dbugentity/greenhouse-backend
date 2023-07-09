4d4f36fe3d48049bc76667f777cf58fae21a8537
vishwa <vishwa.goghari@kinship.co>
2023-07-09 00:03:41 -0400

    try to make custom paralax scroll

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 // @ts-nocheck
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import JobBoard from "./components/Greenhouse/JobBoard";
 import JobApplication from './components/Greenhouse/JobApplication';
 import Introduction from './components/Greenhouse/Introduction/Introduction';
 import JobList from './components/JobList';
+import styles from './App.module.scss';
+import bg from './assets/png/bg.png';
 
 function App() {
+  const [offsetY, setOffsetY] = useState(0);
+  const [offsetX, setOffsetX] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="App">
+    <section className="app">
+      
+        <img  style={{ transform: `translateY(-${offsetY * 0.5}px)` }} src={bg} alt="" />
+    
+      {/* <Introduction /> */}
+        
+      
 
-      <section>
-        <Introduction />
-        <JobBoard />
-        {/* <JobApplication /> */}
-        {/* <JobList/> */}
-      </section>
+      {/* <img src={vector} alt="My Image" className={styles.vector} /> */}
+      {/* <p className={styles.scrollWithParent}>We’re The Recruiting Home Of The Kinship Division Of Mars Petcare.</p> */}
+      {/* <h1 className={styles.brandHeadline}>
+        <div className={styles.kinshipPageIcons}>
 
-    </div>
+        </div>
+      </h1> */}
+
+      {/* <img src={vector} alt="My Image" className={styles.vector} /> */}
+
+   {/* <Introduction /> */}
+        
+      {/* <img  style={{ transform: `translateY(${offsetY* 0.7}px)` }} alt="" }></img> */}
+    
+
+    
+      <Introduction />
+      {/* <JobApplication /> */}
+      <div 
+      // className={styles.ul}
+      style={{ transform: `translateY(${offsetY* 0.2}px)`, background: " #1A1A1A" }}>
+
+
+    
+      </div>
+      
+        {/* <img  style={{ transform: `translateY(-${offsetY * -0.7}px)` }} src={bg} alt="" /> */}
+
+   
+    </section>
   );
 }
 
--- src/components/Greenhouse/Introduction/Introduction.js
+++ src/components/Greenhouse/Introduction/Introduction.js
@@ -1,26 +1,31 @@
 // @ts-nocheck
-import React, { useState } from 'react';
+import React, { Children, useState, useRef, useEffect } from 'react';
 import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import { animated, useSpring } from '@react-spring/web';
 import styles from './Introduction.module.scss'; 
-import kinshipPageIcons from '../../../assets/png/kinshipPageIcons.png';
+import bg from '../../../assets/png/bg.png';
 import vector from '../../../assets/png/vector.png';
+import {motion, useAnimation, useInView, useScroll, useTransform} from 'framer-motion';
 
 
+const Introduction = ({children, width = "fit-content"}) => {
+  let {scrollYProgress} = useAnimation(['0%', '100%']);
+  let y = useTransform( [0, 5], ["0%", "100%"]);
 
-const Introduction = () => {
+  const ref = useRef(null);
+  const isInView= useInView(ref, {once: true});
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if(isInView) {
+      mainControls.start("visible");
+    };
+  }, [isInView]);
   
   const [selectedDepartment, setSelectedDepartment] = useState('All Departments');
   const [selectedOffice, setSelectedOffice] = useState('All Offices');
   const [jobs, setJobs] = useState([]); // This should be initial jobs array
-
-  // <><Parallax y={[20, -20]}>
-  //   <img src={vector} alt="My Image" className={styles.vector} />
-  // </Parallax><Parallax y={[50, -50]}>
-  //     <img src={vector} alt="My Image" className={styles.vector} />
-  //   </Parallax><Parallax y={[20, -20]}>
-  //     <img src={vector} alt="My Image" className={styles.vectorTwo} />
-  //   </Parallax></>
 
   const departments = ['All Departments', 'Kinship', 'Platform - Brand & Marketing', 'Platform - Technology'];
   const offices = ['All Offices', 'New York', 'San Francisco', 'Portland', 'London'];
@@ -45,123 +50,26 @@ const Introduction = () => {
     if (office !== 'All Offices') {
       filteredJobs = filteredJobs.filter(job => job.office === office);
     }
-
     setJobs(filteredJobs);
   };
-
-  const animationProps = useSpring({
-    from: { opacity: 0, marginTop: -500 },
-    to: { opacity: 1, marginTop: 0 },
-    config: { duration: 1000 },
-  });
-
   return (
-//     <ParallaxProvider>
-  
-//     <div>
-//       <header className={styles.header}>
-//         <div className={styles.logoWrapper}>
-//           <h1 className={styles.logo}>R1</h1>
+    <div>
+   
+<motion.div 
+variants={{
+  hidden: {opacity: 0, y: 75},
+  visible: {opacity: 1, y: 0}
+}}
+initial='hidden'
+animate={mainControls}
+transition={{duration: 0.5, delay: 0.25} }
+>
+</motion.div>
+{/* <section className='w-escape relative -mt-[76px] flex flex-col pt -[76px]'>
 
-//         </div>
+</section> */}
 
-//         <div className={styles.headerGrid}>
-//           <h3 className={styles.subtitle}></h3>
-//         </div>
-
-//       </header>
-//       {/* <img src={kinshipPageIcons} alt="Kinship Page Icons" className={styles.kinshipPageIcons} /> */}
-
-//       {/* <img src={vector} alt="My Image" className={styles.vector} /> */}
-//       {/* <img src={kinshipPageIcons} alt="Kinship Page Icons" className={styles.kinshipPageIcons} /> */}
-
-//       <img src={vector} alt="My Image" className={styles.vector} />
-//       <p className={styles.scrollWithParent}>We’re The Recruiting Home Of The Kinship Division Of Mars Petcare.</p>
-
-//       <h1 className={styles.brandHeadline}>
-
-//         <div className={styles.kinshipPageIcons}>
-//           A whole new world.
-//         </div>
-//       </h1>
-
-//       <img src={vector} alt="My Image" className={styles.vector} />
-
-//       <h2 className={styles.bodyCopy}>We also recruit for at-home diagnostics brands Wisdom and Whistle.</h2>
-//       <h2 className={styles.bodyCopyThree}>(part of the Science & Diagnostics division)</h2>
-//       <div >
-//         <h3 className={styles.bodyCopyTwo}>
-//           Both Kinship and Science & Diagnostics are divisions of Mars Petcare—and are dedicated to one purpose: creating A BETTER WORLD FOR PETS™. Through comprehensive veterinary care, nutrition, breakthrough programs in diagnostics, wearable health monitoring, DNA testing and pet welfare, our 100,000 Petcare Associates help pets in more than 130 countries.
-//         </h3>
-//       </div>
-
-//       <p className={styles.bodyCopy}>Interested? Learn more about our open roles below.</p>
-
-//       <img src={vector} alt="My Image" className={styles.vectorTwo} />
-
-//       <h4 className={styles.bodyCopyFour}>Our Family Of Brands</h4>
-
-//       <ul className={styles.ul}>
-//   <li><a href="#">The Kin</a></li>
-//   <li><a href="#">The Wildest</a></li>
-//   <li><a href="#">PetExec</a></li>
-//   <li><a href="#">Adopt a Pet</a></li>
-//   <li><a href="#">VetInsight</a></li>
-//   <li><a href="#">Whistle Labs</a></li>
-//   <li><a href="#">Wisdom Panel</a></li>
-// </ul>
-
-//       {/* <div className={styles['side-column']}>
-//   <p className={styles.bodyCopyTwo}>
-//     The Kin, The Wildest, PetExec, VetInsight, Adopt a Pet<br />
-//     Part of Mars Petcare’s Kinship Division
-//   </p>
-//   <p className={styles.bodyCopyTwo}>
-//     Whistle Labs and Wisdom Panel<br />
-//     Part of Mars Petcare’s Science & Diagnostics Division
-//   </p>
-// </div> */}
-
-
-
-//       <div id="job-board-filter" className={styles['dropdown-container']}>
-//         <div>
-//           <label>Department: </label>
-//           <select value={selectedDepartment} onChange={handleDepartmentChange} className={styles['dropdown']}>
-//             {departments.map((department, index) => (
-//               <option key={index} value={department}>
-//                 {department}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-
-//         <div>
-//           <label>All Offices: </label>
-//           <select value={selectedOffice} onChange={handleOfficeChange} className={styles['dropdown']}>
-//             {offices.map((office, index) => (
-//               <option key={index} value={office}>
-//                 {office}
-//               </option>
-//             ))}
-//           </select>
-//         </div>
-//       </div>
-
-//       <ul>
-//         {jobs.map(job => (
-//           <li key={job.id}>
-//             {job.title} - {job.department} - {job.office}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//     </ParallaxProvider>
-
-<ParallaxProvider>
-<animated.div style={animationProps}>
   <div>
-    <ParallaxProvider y={[-20, 20]}>
       <header className={styles.header}>
         <div className={styles.logoWrapper}>
           <h1 className={styles.logo}>R1</h1>
@@ -177,11 +85,10 @@ const Introduction = () => {
           A whole new world.
         </div>
       </h1>
-      {/* more components */}
+
       <img src={vector} alt="My Image" className={styles.vector} />
-      </ParallaxProvider>
 
-
+</div>
 <h2 className={styles.bodyCopy}>We also recruit for at-home diagnostics brands Wisdom and Whistle.</h2>
 <h2 className={styles.bodyCopyThree}>(part of the Science & Diagnostics division)</h2>
 <div >
@@ -192,12 +99,15 @@ const Introduction = () => {
 
 <p className={styles.bodyCopy}>Interested? Learn more about our open roles below.</p>
 
-<img src={vector} alt="My Image" className={styles.vectorTwo} />
+<img src={bg} alt="My Image" className={styles.vectorTwo} />
 
 <h4 className={styles.bodyCopyFour}>Our Family Of Brands</h4>
+<h5 className={styles.copy} style={{padding: "0"}}>Discover the wonders and surprises that await you as you hover overthem!</h5>
 
 <ul className={styles.ul}>
-<li><a href="#">The Kin</a></li>
+<li><a href="#">The Kin</a>
+<img src={bg} alt="" className={styles.ulImg} style={{display: "none"}}/>
+</li>
 <li><a href="#">The Wildest</a></li>
 <li><a href="#">PetExec</a></li>
 <li><a href="#">Adopt a Pet</a></li>
@@ -238,10 +148,6 @@ const Introduction = () => {
         ))}
       </ul>
     </div>
-   
-
-</animated.div>
-</ParallaxProvider>
   );
 };
 
