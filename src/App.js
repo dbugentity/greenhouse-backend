cd4f50f52a29d2c30f85bdcf6031e464bc21d943
vishwa <vishwa.goghari@kinship.co>
2023-07-09 18:30:09 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from "react";
 import "./App.scss";
 import bg from './assets/png/bg.png';
 import Introduction from "./components/Greenhouse/Introduction/Introduction";
+import JobApplication from "./components/Greenhouse/JobApplication";
+import JobBoard from "./components/Greenhouse/JobBoard";
+import JobList from "./components/Greenhouse/JobList";
 
 function App() {
   const [offsetY, setOffsetY] = useState(0);
@@ -16,13 +19,21 @@ function App() {
 
   return (
     <section>
-      <Introduction  style={{ transform: `translateY(${offsetY * 0.2}px)`}}></Introduction>
-      <div
-      
-      >
-           <img  style={{ transform: `translateY(${offsetY * 0.2}px)`}} src={bg} alt=""></img>
-      </div>
+      <header className="site-header" style={{ transform: `translateY(${offsetY * 0.5}px)` }}>
+        <div className="site-header-mask">
+        </div>
+      </header>
 
+      <Introduction style={{marginTop: "2%", transform: `translateY(${offsetY * 0.3}px)`}}></Introduction>
+
+      <div style={{marginTop: "2%", transform: `translateY(${offsetY * 0.3}px)`}}>
+        <JobApplication/>
+      </div>
+      
+
+      <JobBoard style={{ transform: `translateY(${offsetY * 1.1}px)`}}/>
+        <JobList/>
+      
     </section>
   );
 }
