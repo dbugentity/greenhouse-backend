2cb380ae80cf3a3caba2285cdc6d8d8cef9c0ae2
vishwa <vishwa.goghari@kinship.co>
2023-07-10 12:48:00 -0400

    display current jobs using job board api

--- src/components/Greenhouse/JobList.js
+++ src/components/Greenhouse/JobList.js
@@ -1,84 +1,532 @@
 // @ts-nocheck
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+// // @ts-nocheck
+// // // @ts-nocheck
+// // // // @ts-nocheck
 
-function JobList() {
+
+// // // import React, { useEffect, useState } from 'react';
+// // // import axios from 'axios';
+
+// // // const JobList = () => {
+// // //   const [jobs, setJobs] = useState([]);
+// // //   const [departments, setDepartments] = useState([]);
+// // //   const [offices, setOffices] = useState([]);
+// // //   const [selectedDepartment, setSelectedDepartment] = useState('');
+// // //   const [selectedOffice, setSelectedOffice] = useState('');
+
+// // //   useEffect(() => {
+// // //     const fetchJobs = async () => {
+// // //       try {
+// // //         const response = await axios.get(
+// // //           'https://boards-api.greenhouse.io/v1/boards/{board_token}/jobs?content=true'
+// // //         );
+// // //         const fetchedJobs = response.data.jobs;
+// // //         setJobs(fetchedJobs);
+
+// // //         // Extract unique departments and offices from job data
+// // //         const uniqueDepartments = Array.from(
+// // //           new Set(fetchedJobs.map((job) => job.metadata.department))
+// // //         );
+// // //         setDepartments(uniqueDepartments);
+
+// // //         const uniqueOffices = Array.from(
+// // //           new Set(fetchedJobs.map((job) => job.metadata.office))
+// // //         );
+// // //         setOffices(uniqueOffices);
+// // //       } catch (error) {
+// // //         console.error('Error fetching jobs:', error);
+// // //       }
+// // //     };
+
+// // //     fetchJobs();
+// // //   }, []);
+
+// // //   const handleDepartmentChange = (event) => {
+// // //     setSelectedDepartment(event.target.value);
+// // //   };
+
+// // //   const handleOfficeChange = (event) => {
+// // //     setSelectedOffice(event.target.value);
+// // //   };
+
+// // //   const filteredJobs = jobs.filter((job) => {
+// // //     if (selectedDepartment && selectedDepartment !== 'All' && job.metadata.department !== selectedDepartment) {
+// // //       return false;
+// // //     }
+// // //     if (selectedOffice && selectedOffice !== 'All' && job.metadata.office !== selectedOffice) {
+// // //       return false;
+// // //     }
+// // //     return true;
+// // //   });
+
+// // //   return (
+// // //     <div>
+// // //       {jobs.length === 0 ? (
+// // //         <p>Loading...</p>
+// // //       ) : (
+// // //         <ul>
+// // //           {jobs.map((job) => (
+// // //             <li key={job.id}>
+// // //               <h4>{job.title}</h4>
+// // //               {job.metadata && job.metadata.department && (
+// // //                 <p>Department: {job.metadata.department}</p>
+// // //               )}
+// // //               {job.metadata && job.metadata.office && (
+// // //                 <p>Office: {job.metadata.office}</p>
+// // //               )}
+// // //               {/* {job.content && (
+// // //                 <div dangerouslySetInnerHTML={{ __html: job.content }} />
+// // //               )} */}
+// // //               <button>Apply Now</button>
+// // //             </li>
+// // //           ))}
+// // //         </ul>
+// // //       )}
+// // //     </div>
+// // //   );
+// // // };
+
+// // // export default JobList;
+
+// // import React, { useEffect, useState } from 'react';
+// // import axios from 'axios';
+
+// // const JobList = ({ selectedDepartment, selectedOffice }) => {
+// //   const [jobs, setJobs] = useState([]);
+
+// //   useEffect(() => {
+// //     const fetchJobs = async () => {
+// //       try {
+// //         const response = await axios.get(
+// //           'https://boards-api.greenhouse.io/v1/boards/kinship/jobs?content=true'
+// //         );
+// //         setJobs(response.data.jobs);
+// //       } catch (error) {
+// //         console.error('Error fetching jobs:', error);
+// //       }
+// //     };
+
+// //     fetchJobs();
+// //   }, []);
+
+// //   const filteredJobs = jobs.filter((job) => {
+// //     if (selectedDepartment && job.metadata?.department !== selectedDepartment) {
+// //       return false;
+// //     }
+
+// //     if (selectedOffice && job.metadata?.office !== selectedOffice) {
+// //       return false;
+// //     }
+
+// //     return true;
+// //   });
+
+// //   return (
+// //     <div>
+// //       <h2>Current Job Openings</h2>
+// //       {filteredJobs.length === 0 ? (
+// //         <p>No jobs found.</p>
+// //       ) : (
+// //         <ul>
+// //           {filteredJobs.map((job) => (
+// //             <li key={job.id}>
+// //               <h3>{job.title}</h3>
+// //               {job.metadata && job.metadata.department && (
+// //                 <p>Department: {job.metadata.department}</p>
+// //               )}
+// //               {job.metadata && job.metadata.office && (
+// //                 <p>Office: {job.metadata.office}</p>
+// //               )}
+// //               {/* {job.content && (
+// //                 <div dangerouslySetInnerHTML={{ __html: job.content }} />
+// //               )} */}
+// //               <button>Apply Now</button>
+// //             </li>
+// //           ))}
+// //         </ul>
+// //       )}
+// //     </div>
+// //   );
+// // };
+
+// // const JobBoard = () => {
+// //   const [departments, setDepartments] = useState([]);
+// //   const [offices, setOffices] = useState([]);
+// //   const [selectedDepartment, setSelectedDepartment] = useState('');
+// //   const [selectedOffice, setSelectedOffice] = useState('');
+
+// //   useEffect(() => {
+// //     const fetchDepartmentsAndOffices = async () => {
+// //       try {
+// //         const response = await axios.get(
+// //           'https://boards-api.greenhouse.io/v1/boards/kinship/jobs'
+// //         );
+// //         const { jobs } = response.data;
+// //         const uniqueDepartments = Array.from(
+// //           new Set(jobs.map((job) => job.metadata?.department))
+// //         ).filter(Boolean);
+// //         const uniqueOffices = Array.from(
+// //           new Set(jobs.map((job) => job.metadata?.office))
+// //         ).filter(Boolean);
+// //         setDepartments(uniqueDepartments);
+// //         setOffices(uniqueOffices);
+// //       } catch (error) {
+// //         console.error('Error fetching departments and offices:', error);
+// //       }
+// //     };
+
+// //     fetchDepartmentsAndOffices();
+// //   }, []);
+
+// //   const handleDepartmentChange = (event) => {
+// //     setSelectedDepartment(event.target.value);
+// //     setSelectedOffice('');
+// //   };
+
+// //   const handleOfficeChange = (event) => {
+// //     setSelectedOffice(event.target.value);
+// //     setSelectedDepartment('');
+// //   };
+
+// //   return (
+// //     <div>
+// //       <h1 style={{ marginBottom: '0px' }}>Current Job Openings</h1>
+
+// //       <div id="job-board-filter">
+// //         <div>
+// //           <label>Department: </label>
+// //           <select
+// //             value={selectedDepartment}
+// //             onChange={handleDepartmentChange}
+// //           >
+// //             <option value="">All Departments</option>
+// //             <option value="">Kinship</option>
+// //             <option value="">Platform - Brand & Marketing</option>
+// //             <option value="">Platform - Technology</option>
+// //             {departments.map((department, index) => (
+// //               <option key={index} value={department}>
+// //                 {department}
+// //               </option>
+// //             ))}
+// //           </select>
+// //         </div>
+
+// //         <div>
+// //           <label>All Offices: </label>
+// //           <select value={selectedOffice} onChange={handleOfficeChange}>
+// //             <option value="">All Offices</option>
+// //             {offices.map((office, index) => (
+// //               <option key={index} value={office}>
+// //                 {office}
+// //               </option>
+// //             ))}
+// //           </select>
+// //         </div>
+// //       </div>
+
+// //       <JobList selectedDepartment={selectedDepartment} selectedOffice={selectedOffice} />
+// //     </div>
+// //   );
+// // };
+
+// // export default JobBoard;
+
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// const JobList = ({ selectedDepartment, selectedOffice }) => {
+//   const [jobs, setJobs] = useState([]);
+
+//   useEffect(() => {
+//     const fetchJobs = async () => {
+//       try {
+//         const response = await axios.get(
+//           'https://boards-api.greenhouse.io/v1/boards/kinship/jobs?content=true'
+//         );
+//         setJobs(response.data.jobs);
+//       } catch (error) {
+//         console.error('Error fetching jobs:', error);
+//       }
+//     };
+
+//     fetchJobs();
+//   }, []);
+
+//   const filteredJobs = jobs.filter((job) => {
+//     if (selectedDepartment && selectedDepartment !== 'All' && job.metadata?.department !== selectedDepartment) {
+//       return false;
+//     }
+//     if (selectedOffice && selectedOffice !== 'All' && job.metadata?.office !== selectedOffice) {
+//       return false;
+//     }
+//     return true;
+//   });
+
+//   return (
+//     <div>
+//       <h2>Current Job Openings</h2>
+//       {filteredJobs.length === 0 ? (
+//         <p>No jobs found.</p>
+//       ) : (
+//         <ul>
+//           {filteredJobs.map((job) => (
+//             <li key={job.id}>
+//               <h3>{job.title}</h3>
+//               {job.metadata && job.metadata.department && (
+//                 <p>Department: {job.metadata.department}</p>
+//               )}
+//               {job.metadata && job.metadata.office && (
+//                 <p>Office: {job.metadata.office}</p>
+              
+//               )}
+//               <button>Apply Now</button>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+
+// const JobBoard = () => {
+//   const [departments, setDepartments] = useState([]);
+//   const [offices, setOffices] = useState([]);
+//   const [selectedDepartment, setSelectedDepartment] = useState('');
+//   const [selectedOffice, setSelectedOffice] = useState('');
+
+//   useEffect(() => {
+//     const fetchDepartmentsAndOffices = async () => {
+//       try {
+//         const response = await axios.get(
+//           'https://boards-api.greenhouse.io/v1/boards/kinship/jobs'
+//         );
+//         const { jobs } = response.data;
+//         const uniqueDepartments = Array.from(
+//           new Set(jobs.map((job) => job.metadata?.department))
+//         ).filter(Boolean);
+//         const uniqueOffices = Array.from(
+//           new Set(jobs.map((job) => job.metadata?.office))
+//         ).filter(Boolean);
+//         setDepartments(uniqueDepartments);
+//         setOffices(uniqueOffices);
+//       } catch (error) {
+//         console.error('Error fetching departments and offices:', error);
+//       }
+//     };
+
+//     fetchDepartmentsAndOffices();
+//   }, []);
+
+//   const handleDepartmentChange = (event) => {
+//     setSelectedDepartment(event.target.value);
+//     setSelectedOffice('');
+//   };
+
+//   const handleOfficeChange = (event) => {
+//     setSelectedOffice(event.target.value);
+//     setSelectedDepartment('');
+//   };
+
+//   return (
+//     <div>
+//       <h1 style={{ marginBottom: '0px' }}>Current Job Openings</h1>
+
+//       <div id="job-board-filter">
+//         <div>
+//           <label>Department: </label>
+//           <select
+//             value={selectedDepartment}
+//             onChange={handleDepartmentChange}
+//           >
+//             <option value="">All Departments</option>
+//             <option value="">Kinship</option>
+//             <option value="Platform - Brand & Marketing">Platform - Brand & Marketing</option>
+//             <option value="Platform - Technology">Platform - Technology</option>
+//             {departments.map((department, index) => (
+//               <option key={index} value={department}>
+//                 {department}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+
+//         <div>
+//           <label>All Offices: </label>
+//           <select value={selectedOffice} onChange={handleOfficeChange}>
+//             <option value="">All Offices</option>
+//             {offices.map((office, index) => (
+//               <option key={index} value={office}>
+//                 {office}
+//               </option>
+//             ))}
+//           </select>
+//         </div>
+//       </div>
+
+//       <JobList selectedDepartment={selectedDepartment} selectedOffice={selectedOffice} />
+//     </div>
+//   );
+// };
+
+// export default JobBoard;
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from '../JobList.module.scss';
+
+const JobList = ({ selectedDepartment, selectedOffice, selectedJob, handleJobChange }) => {
   const [jobs, setJobs] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchJobs = async () => {
-      setLoading(true);
       try {
-        const response = await axios.get('https://boards-api.greenhouse.io/v1/boards/{board_token}', {
-          headers: {
-            Accept: 'application/json, text/plain, */*', 'Content-Type': null, board_token: 'whistlelabs',
-          }
-        });
-
-        
-
-//         const response = await axios.get('https://api.greenhouse.io/v1/kinship/jobs', {
-//           headers: {
-//             Accept: 'application/json, text/plain, */*', 'Content-Type': null, board_token: 'whistlelabs',
-//           }
-// });
-        if (response && response) {
-          setJobs(response);
-        } else {
-          throw new Error('Invalid response data');
-        }
-        setLoading(false);
-      } catch (err) {
-        console.error(err);
-        setError(`Error fetching data from Greenhouse API: ${err.message}`);
-        setLoading(false);
+        const response = await axios.get(
+          'https://boards-api.greenhouse.io/v1/boards/kinship/jobs?content=true'
+        );
+        setJobs(response.data.jobs);
+      } catch (error) {
+        console.error('Error fetching jobs:', error);
       }
     };
 
     fetchJobs();
   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
+  const filteredJobs = jobs.filter((job) => {
+    if (
+      selectedDepartment &&
+      selectedDepartment !== 'All' &&
+      job.metadata?.department !== selectedDepartment
+    ) {
+      return false;
+    }
+    if (selectedOffice && selectedOffice !== 'All' && job.metadata?.office !== selectedOffice) {
+      return false;
+    }
+    return true;
+  });
 
   return (
     <div>
-      {jobs.length > 0 ? (
-        jobs.map(job => (
-          <div key={job.id}>
-            <h2>{job.title}</h2>
-            <p>{job.location.name}</p>
-            <a href={job.absolute_url}>Job link</a>
-            {job.content && <div dangerouslySetInnerHTML={{ __html: job.content }} />}
-          </div>
-        ))
+      {filteredJobs.length === 0 ? (
+        <p>No jobs found.</p>
       ) : (
-        <h4>{jobs}</h4>
+        <ul>
+          {filteredJobs.map((job) => (
+            <li key={job.id}>
+              <h3>{job.title}</h3>
+              {job.metadata && job.metadata.department && (
+                <p>Department: {job.metadata.department}</p>
+              )}
+              {job.metadata && job.metadata.office && (
+                <p>Office: {job.metadata.office}</p>
+              )}
+              {/* {job.content && (
+                <div dangerouslySetInnerHTML={{ __html: job.content }} />
+              )} */}
+              <button onClick={() => handleJobChange(job.id)}>Apply Now</button>
+            </li>
+          ))}
+        </ul>
       )}
-         {/* {jobs.length > 0 ? (
-          console.log(jobs),
-            <ul>
-              {jobs.map((jobs, index) => (
-                <li key={index}>{jobs}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>{jobs.length}
-            </p>
-            
-          )} */}
-          
     </div>
-  
   );
-  
-}
+};
 
-export default JobList;
+const JobBoard = () => {
+  const [departments, setDepartments] = useState([]);
+  const [offices, setOffices] = useState([]);
+  const [selectedDepartment, setSelectedDepartment] = useState('');
+  const [selectedOffice, setSelectedOffice] = useState('');
+  const [selectedJob, setSelectedJob] = useState('');
+
+  useEffect(() => {
+    const fetchDepartmentsAndOffices = async () => {
+      try {
+        const response = await axios.get(
+          'https://boards-api.greenhouse.io/v1/boards/kinship/jobs'
+        );
+        const { jobs } = response.data;
+        const uniqueDepartments = Array.from(
+          new Set(jobs.map((job) => job.metadata?.department))
+        ).filter(Boolean);
+        const uniqueOffices = Array.from(
+          new Set(jobs.map((job) => job.metadata?.office))
+        ).filter(Boolean);
+        setDepartments(uniqueDepartments);
+        setOffices(uniqueOffices);
+      } catch (error) {
+        console.error('Error fetching departments and offices:', error);
+      }
+    };
+
+    fetchDepartmentsAndOffices();
+  }, []);
+
+  const handleDepartmentChange = (event) => {
+    setSelectedDepartment(event.target.value);
+    setSelectedOffice('');
+    setSelectedJob('');
+  };
+
+  const handleOfficeChange = (event) => {
+    setSelectedOffice(event.target.value);
+    setSelectedDepartment('');
+    setSelectedJob('');
+  };
+
+  const handleJobChange = (jobId) => {
+    setSelectedJob(jobId);
+  };
+
+  return (
+    <div>
+      <h1 style={{ marginBottom: '0px' }}>Current Job Openings</h1>
+
+      <div id="job-board-filter" className={styles['dropdown-container']}>
+        <div>
+          <label>Department: </label>
+          <select value={selectedDepartment} onChange={handleDepartmentChange}  className={styles['dropdown']} >
+            <option value="">All Departments</option>
+            <option value="Kinship">Kinship</option>
+            <option value="Platform - Brand & Marketing">Platform - Brand & Marketing</option>
+            <option value="Platform - Technology">Platform - Technology</option>
+            {departments.map((department, index) => (
+              <option key={index} value={department}>
+                {department}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div>
+          <label>Office: </label>
+          <select value={selectedOffice} onChange={handleOfficeChange}  className={styles['dropdown']}>
+            <option value="">All Offices</option>
+            {offices.map((office, index) => (
+              <option key={index} value={office}>
+                {office}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      {selectedJob && (
+        <div>
+          <h2>Selected Job</h2>
+          <p>Job ID: {selectedJob}</p>
+        </div>
+      )}
+
+      <JobList
+        selectedDepartment={selectedDepartment}
+        selectedOffice={selectedOffice}
+        selectedJob={selectedJob}
+        handleJobChange={handleJobChange}
+      />
+    </div>
+  );
+};
+
+export default JobBoard;
